46bbe85b9be761c67f302ef04db4f6c6603f4db3
benranderson <ben.randerson@technipfmc.com>
2021-01-20 20:16:33 +0000

    add setup script and requirements file

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -1,5 +1,6 @@
 """Top-level package for {{ cookiecutter.project_name }}."""
 
+__version__ = "{{ cookiecutter.version }}"
+__title__ = "{{ cookiecutter.project_slug }}"
 __author__ = "{{ cookiecutter.full_name }}"
 __email__ = "{{ cookiecutter.email }}"
-__version__ = "{{ cookiecutter.version }}"
