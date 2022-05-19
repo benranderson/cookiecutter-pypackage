745f2fc58229100ad53d4239ab312e6a51457210
benranderson <ben.randerson@technipfmc.com>
2022-05-19 21:20:48 +0100

    parametrize tests

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/module.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/module.py
@@ -1,6 +1,18 @@
 """Example calculation module."""
 
 
-def get_result(a, b):
-    """Example calculation function."""
+def main(a, b, c):
+    """Main calculation function."""
+    result_1 = get_result_1(a, b)
+    result_2 = get_result_2(result_1, c)
+    return {"result_1": result_1, "result_2": result_2}
+
+
+def get_result_1(a, b):
+    """First example calculation function."""
     return a * b
+
+
+def get_result_2(result_1, c):
+    """Second example calculation function."""
+    return result_1 + c
--- {{cookiecutter.project_slug}}/tests/test_module.py
+++ {{cookiecutter.project_slug}}/tests/test_module.py
@@ -6,15 +6,22 @@ import pytest
 from {{ cookiecutter.project_slug }} import module
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def data():
     return {
         "a": np.array([1, 2, 3]),
         "b": np.array([2, 4, 6]),
-        "result": np.array([2, 8, 18]),
+        "c": np.array([1, 1, 1]),
+        "result_1": np.array([2, 8, 18]),
+        "result_2": np.array([3, 9, 19]),
     }
 
 
-def test_get_result(data):
-    actual = module.get_result(data["a"], data["b"])
-    np.testing.assert_equal(actual, data["result"])
+@pytest.fixture(scope="module")
+def results(data):
+    return module.main(data["a"], data["b"], data["c"])
+
+
+@pytest.mark.parametrize("param", ["result_1", "result_2"])
+def test_get_result(param, data, results):
+    np.testing.assert_equal(results[param], data[param])
