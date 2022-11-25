816216470ca0094d38ad336b674e677b94d5c215
benranderson <ben.randerson@technipfmc.com>
2022-11-25 22:50:00 +0000

    documentation improvements

--- {{cookiecutter.project_slug}}/docs/conf.py
+++ {{cookiecutter.project_slug}}/docs/conf.py
@@ -3,18 +3,25 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Imports -----------------------------------------------------------------
+
+import {{cookiecutter.project_slug}}
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "{{cookiecutter.project_slug}}"
 copyright = "2022, {{ cookiecutter.full_name }}"
 author = "{{ cookiecutter.full_name }}"
-release = "{{ cookiecutter.version }}"
+
+# The full version, including alpha/beta/rc tags
+release = {{cookiecutter.project_slug}}.__version__
+version = {{cookiecutter.project_slug}}.__version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "sphinx_click"]
+extensions = ["myst_parser", "sphinx_copybutton", "sphinx_click"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -28,3 +35,21 @@ html_static_path = ["_static"]
 
 # Configure project logo
 html_logo = "_static/logo.svg"
+
+# Configure icon links
+html_theme_options = {
+    "icon_links": [
+        {
+            "name": "Git",
+            "url": "{{ cookiecutter.repo_url }}",
+            "icon": "fab fa-git-alt",
+        }
+    ]
+}
+
+# -- MyST Parser configuration -----------------------------------------------
+
+# auto-generate header anchors
+myst_heading_anchors = 3
+
+myst_enable_extensions = ["colon_fence", "dollarmath"]
